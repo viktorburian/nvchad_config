4c4a34110567e810ef684305433128f8903c0688
Viktor Burian <viktor.burian@macbookpro>
2023-09-27 21:29:01 +0200

    Add nvim surround

--- plugins.lua
+++ plugins.lua
@@ -163,5 +163,14 @@ local plugins = {
             require("mini.animate").setup()
        end,
    },
+   {
+       "kylechui/nvim-surround",
+       version = "*", -- Use for stability; omit to use `main` branch for the latest features
+       event = "VeryLazy",
+       config = function()
+           require("nvim-surround").setup()
+       end
+   },
 }
+
 return plugins
